7c73ef6371b0741bcbf0502f42f1c1ec29d1a1f9
zhaoyuanmeng <1426593075@qq.com>
2022-09-14 12:03:26 +0800

    feat: 添加服务端测试

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+app.post('/reportData', (req: any, res: any) => {
+  console.log(req.body.length)
+  res.status(200).send('')
+})
+
+app.listen(8080, () => {
+  console.log('server listen on port 8080...')
+})
